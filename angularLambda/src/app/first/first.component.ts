8ec7bcdc970dec50e4bf80d88b31f400061a35c4
kseniia_makarova <kseniia.makarova.kyiv@gmail.com>
2020-11-03 11:59:22 +0200

    serverless dependencies and fixed css in components

--- angularLambda/src/app/first/first.component.ts
+++ angularLambda/src/app/first/first.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-  template: '<h1>Hello World!</h1>'
+  templateUrl: `first.component.html`,
+  styleUrls: ['./first.component.css']
 })
 export class FirstComponent {
-
 }
