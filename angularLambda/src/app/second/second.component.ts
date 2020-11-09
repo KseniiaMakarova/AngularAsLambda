dba35e30fca84b0633383444a6a34e480124f6bf
KseniiaMakarova <59459757+KseniiaMakarova@users.noreply.github.com>
2020-11-09 07:23:34 +0200

    Revert "added websocket with `socket-io` library (#2)"
    This reverts commit a1da35c6fffb6c572f3979b38432c85e2fa9199a.

--- angularLambda/src/app/second/second.component.ts
+++ angularLambda/src/app/second/second.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { EchoService } from '../echo.service';
 import { Observable } from 'rxjs';
-import * as io from 'socket.io-client';
 
 @Component({
   templateUrl: `second.component.html`,
@@ -9,29 +8,10 @@ import * as io from 'socket.io-client';
 })
 export class SecondComponent implements OnInit {
   public response: Observable<any>;
-  socket: any;
 
   constructor(private echoService: EchoService) {}
 
   public ngOnInit(): void {
     this.response = this.echoService.makeCall();
-    this.socket = io('https://2bmasodkz9.execute-api.eu-central-1.amazonaws.com/v1/ws/');
-    this.listen('/chat').subscribe(data => console.log("received something!"));
-  }
-
-  listen(eventName: string) {
-    return new Observable((subscriber) => {
-      this.socket.on(eventName, data => {
-        subscriber.next(data)
-      })
-    })
-  }
-
-  emit(eventName, data) {
-    this.socket.emit(eventName, data);
-  }
-
-  public createNewEntity(): void {
-    console.log("button clicked!");
   }
 }
