33c01122ef2963169af3554f0ca0ab25294a2e8c
kseniia_makarova <kseniia.makarova.kyiv@gmail.com>
2020-11-09 07:13:12 +0200

    trying to use `socket-io` library - got cors policy blocker

--- angularLambda/src/app/second/second.component.ts
+++ angularLambda/src/app/second/second.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { EchoService } from '../echo.service';
 import { Observable } from 'rxjs';
+import * as io from 'socket.io-client';
 
 @Component({
   templateUrl: `second.component.html`,
@@ -8,11 +9,26 @@ import { Observable } from 'rxjs';
 })
 export class SecondComponent implements OnInit {
   public response: Observable<any>;
+  socket: any;
 
   constructor(private echoService: EchoService) {}
 
   public ngOnInit(): void {
     this.response = this.echoService.makeCall();
+    this.socket = io('https://2bmasodkz9.execute-api.eu-central-1.amazonaws.com/v1/ws/');
+    this.listen('/chat').subscribe(data => console.log("received something!"));
+  }
+
+  listen(eventName: string) {
+    return new Observable((subscriber) => {
+      this.socket.on(eventName, data => {
+        subscriber.next(data)
+      })
+    })
+  }
+
+  emit(eventName, data) {
+    this.socket.emit(eventName, data);
   }
 
   public createNewEntity(): void {
